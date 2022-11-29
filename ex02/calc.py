f456b5451482874c9fd375919aae7dd8882e5297
kouki0412 <c0a210793a@edu.teu.ac.jp>
2022-11-29 14:48:16 +0900

    大幅に変更し、５が完成

--- ex02/calc.py
+++ ex02/calc.py
@@ -6,16 +6,32 @@ def button_click(event):
     txt = btn["text"]
     tkm.showinfo(txt,f"{txt}が押されました")
 
+def make_button(s):
+    global r,c
+    button = tk.Button(root,text=s,font=("",30),width=4,height=2)
+    button.bind("<1>",button_click)
+    button.grid(row=r,column=c)
+    c += 1
+    if c%4==0:
+        r += 1
+        c = 1
+
 root = tk.Tk()
 root.title("Calc")
 root.geometry("300x500")
 
-entry = tk.Entry(root,justify="right",width=10,font=("",40))
-entry.grid(row=1,column=1,columnspan=3)
 
-for i in range(10):
-    button = tk.Button(root,text=str(i),font=("",30),width=4,height=2)
-    button.bind("<1>",button_click)
-    button.grid(row =(9-i)//3+2,column=(9-i)%3+1)
+r = 1
+c = 1
+
+entry = tk.Entry(root,justify="right",width=10,font=("",40))
+entry.grid(row=r,column=c,columnspan=3)
+
+r += 1
+
+for i in range(9,-1,-1):
+    make_button(str(i))
+make_button("+")
+make_button("=")
 
 root.mainloop()
