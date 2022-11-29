ad02251c04a6b6d35834b95f5e96ccd675130eb3
kouki0412 <c0a210793a@edu.teu.ac.jp>
2022-11-29 14:35:25 +0900

    ４が完成

--- ex02/calc.py
+++ ex02/calc.py
@@ -10,9 +10,12 @@ root = tk.Tk()
 root.title("Calc")
 root.geometry("300x500")
 
+entry = tk.Entry(root,justify="right",width=10,font=("",40))
+entry.grid(row=1,column=1,columnspan=3)
+
 for i in range(10):
     button = tk.Button(root,text=str(i),font=("",30),width=4,height=2)
     button.bind("<1>",button_click)
-    button.grid(row =(9-i)//3+1,column=(9-i)%3+1)
+    button.grid(row =(9-i)//3+2,column=(9-i)%3+1)
 
 root.mainloop()
