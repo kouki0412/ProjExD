68f4ed02d2b608cffb052267f9fa454450fe2979
kouki0412 <c0a210793a@edu.teu.ac.jp>
2022-11-29 15:49:17 +0900

    キーの実装(ｘが連続にならないように等)

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,15 +1,30 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
+consecutive_ok = ["(",")"]
+for i in range(10):
+    consecutive_ok.append(str(i))
+
+tran = {"x":"*","÷":"/","^":"**"}
+
+
 def button_click(event):
     btn = event.widget
     txt = btn["text"]
     if txt == "=":
         que = entry.get()
+        for olds,news in tran.items():
+            que = que.replace(olds,news)
         entry.delete(0,tk.END)
         entry.insert(tk.END,eval(que))
-    else:
+    elif txt=="C":
+        entry.delete(0,tk.END)
+    elif txt in consecutive_ok:
         entry.insert(tk.END,txt)
+    else:
+        que = entry.get()
+        if que[-1] in consecutive_ok:
+            entry.insert(tk.END,txt)
     #tkm.showinfo(txt,f"{txt}が押されました")
 
 def make_button(s):
@@ -35,13 +50,15 @@ entry.grid(row=r,column=c,columnspan=4)
 
 r += 1
 
+#このレイアウトになるようにキーを配置
 key_mat = [
-        ["^","(",")","/"],
-        ["7","8","9","*"],
+        ["^","(",")","÷"],
+        ["7","8","9","x"],
         ["4","5","6","-"],
         ["1","2","3","+"],
         ["0",".","C","="] 
        ]
+
 for key_vec in key_mat:
     for key in key_vec:
         make_button(key)
