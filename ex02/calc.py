13d6cb8070f25e0c2ade33c7705549138e3347d3
kouki0412 <c0a210793a@edu.teu.ac.jp>
2022-11-29 14:21:13 +0900

    ２が完成

--- ex02/calc.py
+++ ex02/calc.py
@@ -4,4 +4,11 @@ import tkinter.messagebox as tkm
 root = tk.Tk()
 root.title("Calc")
 root.geometry("300x500")
+
+button = dict()
+
+for i in range(10):
+    button[str(i)] = tk.Button(root,text=str(i),font=("",30),width=4,height=2)
+    button[str(i)].grid(row =(9-i)//3+1,column=(9-i)%3+1)
+
 root.mainloop()
