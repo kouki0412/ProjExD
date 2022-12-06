6d405f5a59455e2c1351ef4b4061f0916e275bac
kouki0412 <c0a210793a@edu.teu.ac.jp>
2022-12-06 14:25:33 +0900

    こうかとん配置

--- ex03/maze.py
+++ ex03/maze.py
@@ -5,5 +5,9 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
     canvas = tk.Canvas(width=1500,height=900,bg="black")
+    images = tk.PhotoImage(file="fig/0.png")
+
+    cx = 300; cy = 400
+    canvas.create_image(cx,cy,image=images)
     canvas.pack()
     root.mainloop()
