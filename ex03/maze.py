99c0d6a489d08d0eab39034018ae37c133cc1d2f
kouki0412 <c0a210793a@edu.teu.ac.jp>
2022-12-06 14:45:49 +0900

    移動を実装

--- ex03/maze.py
+++ ex03/maze.py
@@ -4,6 +4,23 @@ def key_down(event):
     global key
     key = event.keysym
 
+def key_up(event):
+    global key
+    key = ""
+
+def main_proc():
+    global cx,cy
+    if key == "Up":
+        cy -= 20
+    if key == "Down":
+        cy += 20
+    if key == "Left":
+        cx -= 20
+    if key == "Right":
+        cx += 20
+    canvas.coords("player",cx,cy)
+    root.after(100,main_proc)
+    
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
@@ -11,8 +28,10 @@ if __name__ == "__main__":
     images = tk.PhotoImage(file="fig/0.png")
 
     cx = 300; cy = 400
-    canvas.create_image(cx,cy,image=images)
+    canvas.create_image(cx,cy,image=images,tags="player")
     key = ""
-    root.bind("<KeyRelease>",key_down)
+    root.bind("<KeyPress>",key_down)
+    root.bind("<KeyRelease>",key_up)
+    main_proc()
     canvas.pack()
     root.mainloop()
