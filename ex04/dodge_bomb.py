edf2b5975429798ca9326897ae2b67bc6510ced1
kouki0412 <c0a210793a@edu.teu.ac.jp>
2022-12-13 18:10:08 +0900

    修正 #13

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -6,6 +6,8 @@ import time
 KEYS = [pg.K_UP,pg.K_DOWN,pg.K_LEFT,pg.K_RIGHT]
 DXY = {pg.K_UP:(0,-1),pg.K_DOWN:(0,1),pg.K_LEFT:(-1,0),pg.K_RIGHT:(1,0)}
 
+
+#爆弾を増やす関数
 def make_bombs(scrn_rct,bombs_sfc,bombs_rct,dxs,dys):
     bomb_sfc = pg.Surface((20,20))
     bomb_sfc.set_colorkey((0,0,0))
@@ -18,9 +20,8 @@ def make_bombs(scrn_rct,bombs_sfc,bombs_rct,dxs,dys):
         dxs.append(1)
         dys.append(1)
 
-
+#爆弾を増やす関数
 def erase_bombs(bombs_sfc,bombs_rct,dxs,dys):
-
     if len(bombs_rct)>1:
         bombs_sfc.pop()
         bombs_rct.pop()
@@ -40,6 +41,7 @@ def move_player(scrn_rct,plyr_rct,key_list):
             if plyr_rct.top < scrn_rct.top or plyr_rct.bottom > scrn_rct.bottom:
                 plyr_rct.centery -= Dy
 
+#通常時のBOMBを動かす関数
 def move_bomb1(scrn_rct,bomb_rct,dx,dy):
     if bomb_rct.left < scrn_rct.left or bomb_rct.right > scrn_rct.right:
         dx *= -1
@@ -47,10 +49,9 @@ def move_bomb1(scrn_rct,bomb_rct,dx,dy):
         dy *= -1
     bomb_rct.centerx += dx
     bomb_rct.centery += dy
-    print(dx,dy,"s")
     return dx,dy
 
-
+#自動追尾に切り替わった関数
 def move_bomb2(scrn_rct,bomb_rct,plyr_rct):
     px,py= plyr_rct.center
     bx,by= bomb_rct.center
@@ -89,14 +90,15 @@ def main():
     plyr_rct = plyr_sfc.get_rect()
     plyr_rct.center = 900, 400
 
+    #BOMBのインスタンスをまとめるリスト
     bombs_sfc = []
     bombs_rct = []
+    #それぞれのBOMBが範囲内かどうかを判定するときに使うリスト
     dxs = []
     dys = []
 
     make_bombs(scrn_rct,bombs_sfc,bombs_rct,dxs,dys)
 
-    #clock.tick(0.5)
     while True:
         scrn_sfc.blit(bcsc_sfc,bcsc_rct)
         for event in pg.event.get():
@@ -106,15 +108,17 @@ def main():
 
         move_player(scrn_rct,plyr_rct,key_list)
 
+        #ボタンを押すとBOMBの動きが変わったり増えたりする。
         if key_list[pg.K_1]:
-            mode = 1
+            mode = 1 #通常の動きになる
         elif key_list[pg.K_2]:
-            mode = 2
+            mode = 2 #追尾するようになる
         elif key_list[pg.K_9]:
             make_bombs(scrn_rct,bombs_sfc,bombs_rct,dxs,dys)
         elif key_list[pg.K_8]:
             erase_bombs(bombs_sfc,bombs_rct,dxs,dys)
-
+        
+        #複数のBOMBを順番に処理
         for i,bomb_rct in enumerate(bombs_rct):
             if mode == 1:
                 dxs[i],dys[i] = move_bomb1(scrn_rct,bomb_rct,dxs[i],dys[i])
@@ -137,7 +141,6 @@ def main():
                 pg.display.update()
                 clock.tick(0.5)
                 return
-        print(dxs,dys)
         scrn_sfc.blit(plyr_sfc,plyr_rct)
         for bomb_sfc,bomb_rct in zip(bombs_sfc,bombs_rct):
             scrn_sfc.blit(bomb_sfc,bomb_rct)
