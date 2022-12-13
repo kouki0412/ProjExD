245b83acbbc6e22423b0d7a3279f07ae0c6348af
kouki0412 <c0a210793a@edu.teu.ac.jp>
2022-12-13 14:08:46 +0900

    2完成

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -7,9 +7,19 @@ def main():
 
     bcsc_sfc = pg.image.load("fig/pg_bg.jpg")
     bcsc_rct = bcsc_sfc.get_rect()
-    bcsc_rct.center = 0 , 0
+    bcsc_rct.center = 800 , 450
     clock = pg.time.Clock()
-    clock.tick(0.5)
+    #clock.tick(0.5)
+    while True:
+        scrn_sfc.blit(bcsc_sfc,bcsc_rct)
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return
+        pg.display.update()
+        clock.tick(1000)
+        
+    
+
 
 if __name__ == "__main__":
     pg.init()
