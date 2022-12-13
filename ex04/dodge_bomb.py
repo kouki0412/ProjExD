303351aac85249a7ed6547fc136a2468f688c73b
kouki0412 <c0a210793a@edu.teu.ac.jp>
2022-12-13 14:14:52 +0900

    3完成

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -8,6 +8,11 @@ def main():
     bcsc_sfc = pg.image.load("fig/pg_bg.jpg")
     bcsc_rct = bcsc_sfc.get_rect()
     bcsc_rct.center = 800 , 450
+
+    plyr_sfc = pg.image.load("fig/0.png")
+    plyr_sfc = pg.transform.rotozoom(plyr_sfc,0,2.0)
+    plyr_rct = plyr_sfc.get_rect()
+    plyr_rct.center = 900,400
     clock = pg.time.Clock()
     #clock.tick(0.5)
     while True:
@@ -15,6 +20,7 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
+        scrn_sfc.blit(plyr_sfc,plyr_rct)
         pg.display.update()
         clock.tick(1000)
         
