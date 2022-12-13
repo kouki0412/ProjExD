ca00e8528d3eb74a6e82b7d622b2a30d15bd1a33
kouki0412 <c0a210793a@edu.teu.ac.jp>
2022-12-13 14:39:29 +0900

    ５完成

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,5 +1,6 @@
 import pygame as pg
 import sys
+import random
 
 def main():
     pg.display.set_caption("逃げろ!こうかとん")
@@ -8,11 +9,17 @@ def main():
     bcsc_sfc = pg.image.load("fig/pg_bg.jpg")
     bcsc_rct = bcsc_sfc.get_rect()
     bcsc_rct.center = 800 , 450
-    px, py = 900, 400
+
     plyr_sfc = pg.image.load("fig/0.png")
     plyr_sfc = pg.transform.rotozoom(plyr_sfc,0,2.0)
     plyr_rct = plyr_sfc.get_rect()
+    px, py = 900, 400
     plyr_rct.center = px,py
+
+    bomb_sfc = pg.Surface((100,100))
+    bomb_sfc.set_colorkey((0,0,0))
+    pg.draw.circle(bomb_sfc,(255,0,0),(50,50),10)
+    bx, by = (random.randint(0,1600),random.randint(0,900))
     clock = pg.time.Clock()
     #clock.tick(0.5)
     while True:
@@ -31,6 +38,7 @@ def main():
             px += 1
         plyr_rct.center = px,py
         scrn_sfc.blit(plyr_sfc,plyr_rct)
+        scrn_sfc.blit(bomb_sfc,(bx,by))
         pg.display.update()
         clock.tick(1000)
         
